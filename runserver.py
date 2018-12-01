a71e243d45d9c3a3745a4b3d347d96e7d6d5f94a
Ahmed Khalifa <medoduh@gmail.com>
2018-12-01 18:51:52 +0200

    more lines

--- runserver.py
+++ runserver.py
@@ -5,8 +5,19 @@ from iEgypt import app
 from iEgypt.config import config
 
 
+def set_database_procedures():
+    from iEgypt.model.db import get_conn
+    conn = get_conn()
+    cursor = conn.cursor()
+    with open('dump.sql') as dump:
+        for command in dump.read().split('GO'):
+            cursor.execute(command)
+    conn.close()
+
+
 if __name__ == '__main__':
     hostname = config.get('hostname')
     port = config.get('port')
     app.secret_key = 'secret'
+    set_database_procedures()
     app.run(hostname, port)
