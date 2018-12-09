bf052349239ab86421b8eea4ccbeb7c4469cf3cc
omdosdev <omdos1998@gmail.com>
2018-12-09 02:41:01 +0200

    upload original content for contributor

--- iEgypt/model/db/contributor_model.py
+++ iEgypt/model/db/contributor_model.py
@@ -1,10 +1,15 @@
 from . import get_conn
 
 def upload_oc(id,params):
+
+    #Validate data
+    sql = "SELECT * FROM [subcategory] WHERE name = {subcategory name}".format(**params)
+    
+
     sql= """\
     -- Create new entry for content
     INSERT INTO "Content" ([uploaded at], [contributor id], [category type], [subcategory name], type, link)
-    VALUES (CURRENT_TIMESTAMP, {id}, '{category id}', '{subcategory name}', '{type id}', '{link}');
+    VALUES (CURRENT_TIMESTAMP, {id}, '{category type}', '{subcategory name}', '{type id}', '{link}');
     -- Get the id of the new content
     DECLARE @content_id INTEGER = (SELECT max(id) FROM content)
     -- Create new entry for original content
