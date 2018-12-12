e6817397a45891b472e255df9628e260dbfcb0df
omdosdev <omdos1998@gmail.com>
2018-12-12 15:01:01 +0200

    new stuff for contrib

--- iEgypt/blueprints/contributor.py
+++ iEgypt/blueprints/contributor.py
@@ -1,8 +1,8 @@
 #TBD
-from flask import session, Blueprint, request, url_for, flash, g
-from iEgypt.model.auth import login_required, account_type_required
-from iEgypt.model.db import contributor_model
+from flask import Blueprint, session, request, redirect, url_for, flash, g
 from iEgypt.model.overloaded import load_template
+from iEgypt.model.db import contributor_model
+from iEgypt.model.auth import login_required, account_type_required
 
 #Create the blueprint
 bp = Blueprint('contributor', __name__, url_prefix='/')
@@ -30,12 +30,36 @@ def upload_oc():
 
 
 @bp.route('/upload-nc', methods=('GET', 'POST'))
+@login_required
 def upload_nc():
 
     if request.method == 'POST':
          params = dict()
          for key, val in request.form:
              params[key]=val
+         user_id=session.get('user_id')
          contributor_model.upload_nc(params)
 
     return load_template('contributor/upload-nc.html', title='Upload New Content')
+
+
+@bp.route('/new-requests')
+@login_required
+def show_nr():
+    col_names= ['id','accept status',  'specified', 'information', 'viewer id',
+     'notif obj id', 'contributor_id']
+    user_id=session.get('user_id')
+    table=contributor_model.show_nr()
+
+    return load_template('contributor/new-requests.html', title='New Requests',
+     table=table, col_names=col_names)
+
+
+@bp.route('/show-events')
+@login_required
+def show_events():
+    col_names=['full name','id','description','location','city','time','entertainer',
+    'notification object id']
+    table=contributor_model.show_events()
+    return load_template('contributor/show-events.html', title= 'Show Events',
+     table=table, col_names=col_names)
--- iEgypt/model/db/contributor_model.py
+++ iEgypt/model/db/contributor_model.py
@@ -4,11 +4,11 @@ def upload_oc(id,params):
 
     #Validate data
     sql = "SELECT * FROM [subcategory] WHERE name = {subcategory name}".format(**params)
-    
+
 
     sql= """\
     -- Create new entry for content
-    INSERT INTO "Content" ([uploaded at], [contributor id], [category type], [subcategory name], type, link)
+    INSERT INTO "content" ([uploaded at], [contributor id], [category type], [subcategory name], type, link)
     VALUES (CURRENT_TIMESTAMP, {id}, '{category type}', '{subcategory name}', '{type id}', '{link}');
     -- Get the id of the new content
     DECLARE @content_id INTEGER = (SELECT max(id) FROM content)
@@ -20,3 +20,59 @@ def upload_oc(id,params):
     with get_conn() as conn:
       cursor = conn.cursor()
       cursor.execute(sql)
+
+
+def upload_nc(id,params):
+
+    sql="""\
+     -- Create entry in content
+    INSERT INTO "content" ([uploaded at], [contributor id], [category type], [subcategory name], type, link)
+    VALUES (CURRENT_TIMESTAMP, {id}, '{category type}', '{subcategory name}', '{type id}', '{link}');
+    -- Get the id of the new content
+    DECLARE @content_id INTEGER = (SELECT max(ID) FROM content)
+    -- Create entry in new content
+    INSERT INTO [new content] (id, new request id) VALUES (@content_id, @new_request_id);
+    """
+
+    sql=sql.format(id=id, **params)
+    with get_conn() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+
+
+def show_nr(request_id, contributor_id):
+    sql="""\
+    SELECT * []
+    FROM [new request]
+    WHERE ([contributor id] = {contributor_id} OR specified = 0)\
+  """
+
+  if request_id:
+      sql += 'AND id={request_id}'.format(request_id=request_id)
+  sql.format(contributor_id=contributor_id)
+
+    
+
+
+def show_events(id=None):
+    sql="""\
+    SELECT ([first name]+''+[middle name]+''+[last name]) AS 'full name', e.id,
+ e.description, e.location, e.city, e.time, e.entertainer,e.[notification object id]
+        FROM [event] e
+               INNER JOIN [user] u ON e.[viewer id] = u.id
+        WHERE e.time > CURRENT_TIMESTAMP
+    """
+
+    if id:
+        sql += ' AND e.id={id}'.format(id=id)
+
+    with get_conn() as conn:
+        cursor = conn.cursor().execute(sql)
+        try:
+            columns = [column[0] for column in cursor.description]
+            result = []
+            for row in cursor.fetchall():
+                result.append(dict(zip(columns,row)))
+            return result
+        except Exception:
+            return None
